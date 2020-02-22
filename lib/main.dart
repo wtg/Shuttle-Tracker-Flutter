f97715a149ceaff8dbca830104eaa99ff44d1b64
Samuel Obe <obes@rpi.edu>
2020-02-22 14:40:59 -0500

    Added SafeArea and changed Android settings
    Android settings were changed to ask for access to location service

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './pages/SettingsPage.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+
     @override
     State<StatefulWidget> createState() {
         return MyAppState();
@@ -13,12 +14,14 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+
     int _selectedTab = 0;
     final _pageOptions = [
         MapPage(),
         SchedulesPage(),
         SettingsPage(),
     ];
+
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
@@ -28,34 +31,33 @@ class MyAppState extends State<MyApp> {
                 title: TextStyle(color: Colors.white),
               )
             ),
-            home: Scaffold(
-            body: _pageOptions[_selectedTab],
-            bottomNavigationBar: BottomNavigationBar(
-                currentIndex: _selectedTab,
-                onTap: (int index) {
-                    setState(() {
-                        _selectedTab = index;
-                    });
-                },
-                items: [
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.near_me),
-                        title: Text('Map'),
-                    ),
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.list),
-                        title: Text('Schedules'),
-                    ),
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.settings),
-                        title: Text('Settings'),
-                    ),
-                ],
-            ),
-        ),
-    );
-}}
-
-
-
-
+            home: SafeArea( 
+                child: Scaffold(
+                    body: _pageOptions[_selectedTab],
+                    bottomNavigationBar: BottomNavigationBar(
+                        currentIndex: _selectedTab,
+                        onTap: (int index) {
+                            setState(() {
+                                _selectedTab = index;
+                            });
+                        },
+                        items: [
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.near_me),
+                                title: Text('Map'),
+                            ),
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.list),
+                                title: Text('Schedules'),
+                            ),
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.settings),
+                                title: Text('Settings'),
+                            )
+                        ],
+                    )
+                ),
+            )
+        );
+    }
+}

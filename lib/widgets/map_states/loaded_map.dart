d9b4e9f4dfff72769c5c7212ce4477bb5f5be2f2
Samuel Obe <obes@rpi.edu>
2020-04-22 19:34:58 -0400

    Added stop zoom functionality

--- lib/widgets/map_states/loaded_map.dart
+++ lib/widgets/map_states/loaded_map.dart
@@ -35,8 +35,42 @@ class LoadedMap extends StatefulWidget {
   _LoadedMapState createState() => _LoadedMapState();
 }
 
-class _LoadedMapState extends State<LoadedMap> {
+class _LoadedMapState extends State<LoadedMap> with TickerProviderStateMixin {
   MapController mapController = MapController();
+
+    void _animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    final _latTween = Tween<double>(
+        begin: mapController.center.latitude, end: destLocation.latitude);
+    final _lngTween = Tween<double>(
+        begin: mapController.center.longitude, end: destLocation.longitude);
+    final _zoomTween = Tween<double>(begin: mapController.zoom, end: destZoom);
+
+    // Create a animation controller that has a duration and a TickerProvider.
+    var controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    // The animation determines what path the animation will take. You can try different Curves values, although I found
+    // fastOutSlowIn to be my favorite.
+    Animation<double> animation =
+        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+    controller.addListener(() {
+      mapController.move(
+          LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)),
+          _zoomTween.evaluate(animation));
+    });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.dispose();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.dispose();
+      }
+    });
+
+    controller.forward();
+  }
 
   List<Polyline> _createRoutes(List<dynamic> routesJSON, List<int> _ids,
       Map<String, ShuttleImage> _mapkey, Map<int, Color> _colors) {
@@ -74,9 +108,7 @@ class _LoadedMapState extends State<LoadedMap> {
             builder: (ctx) => Container(
                 child: GestureDetector(
                     onTap: () {
-                      mapController.onReady.then((result) {
-                        mapController.move(stop.getLatLng, 14);
-                      });
+                      _animatedMapMove(stop.getLatLng, 18.0);
                       print('Stop ${stop.name} clicked on');
                     },
                     child: Image.asset('assets/img/circle.png')))));
@@ -131,6 +163,7 @@ class _LoadedMapState extends State<LoadedMap> {
           /// Map
           Flexible(
             child: FlutterMap(
+              mapController: mapController,
               options: MapOptions(
                 center: LatLng(42.731, -73.6767),
                 zoom: 14,

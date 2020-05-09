817bdd4150f2713fc37820138fe5ecbcd0cf8a1f
Samuel Obe <obes@rpi.edu>
2020-05-09 15:35:32 -0400

    Removed size value for iOS progress indicators

--- lib/pages/map_page/map_states/loading_map.dart
+++ lib/pages/map_page/map_states/loading_map.dart
@@ -12,7 +12,7 @@ class _LoadingMapState extends State<LoadingMap> {
   Widget build(BuildContext context) {
     return Center(
       child: PlatformCircularProgressIndicator(
-        ios: (_) => CupertinoProgressIndicatorData(radius: 15),
+        ios: (_) => CupertinoProgressIndicatorData(),
       ),
     );
   }

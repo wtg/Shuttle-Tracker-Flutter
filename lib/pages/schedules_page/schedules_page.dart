45c6b60f876d6b9677ad68d55e1752228ef576c1
Samuel Obe <obes@rpi.edu>
2020-09-09 12:32:17 -0400

    Changed "widgets" folder to "global_widgets"

--- lib/pages/schedules_page/schedules_page.dart
+++ lib/pages/schedules_page/schedules_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../blocs/theme/theme_bloc.dart';
 

a05d7ae55ee184feb465c7e398303f50c661edd9
Samuel Obe <obes@rpi.edu>
2020-09-29 18:14:53 -0400

    Created a new cubit for shuttle state management

---
+++ lib/blocs/shuttles/shuttles_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'shuttles_state.dart';
+
+class ShuttlesCubit extends Cubit<ShuttlesState> {
+  ShuttlesCubit() : super(ShuttlesInitial());
+}

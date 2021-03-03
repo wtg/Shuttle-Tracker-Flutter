c09395a54f704aeb73e754c42665957e0f9685d1
Samuel Obe <samuel.obe@outlook.com>
2021-03-03 17:51:11 -0500

    Removed error causing depedencies

--- lib/widgets/general_settings.dart
+++ lib/widgets/general_settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/theme_bloc/theme_bloc.dart';
@@ -31,18 +30,17 @@ class GeneralSettings extends StatelessWidget {
         ),
         title: Text('Dark Mode',
             style: TextStyle(color: theme.getTheme.hoverColor, fontSize: 16)),
-        trailing: PlatformSwitch(
+        //TODO: Add cupertino switch
+        trailing: Switch(
           value: isSwitched,
           onChanged: (value) {
             isSwitched = value;
             themeBloc.add(ThemeEvent.toggle);
+
           },
           activeColor: Colors.white,
-          material: (context, _) =>
-              MaterialSwitchData(activeTrackColor: Colors.green),
-          cupertino: (context, _) =>
-              CupertinoSwitchData(activeColor: Colors.green),
         ),
+      
       ),
     ]);
   }

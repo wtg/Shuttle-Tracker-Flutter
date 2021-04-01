8a93d26ee84df6eb0142aa2e3d081a9b1c12e8b1
Matt Czyr <mattczyr@gmail.com>
2021-04-01 00:38:05 -0400

    Create tests for fetching routes, stops, updates, and checking the connection through the API provider

---
+++ test/provider_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_shuttletracker/data/models/shuttle_route.dart';
+import 'package:flutter_shuttletracker/data/models/shuttle_stop.dart';
+import 'package:flutter_shuttletracker/data/models/shuttle_update.dart';
+import 'package:flutter_shuttletracker/data/provider/shuttle_api_provider.dart';
+
+void main() {
+  final provider = ShuttleApiProvider();
+  group(
+    'Provider tests',
+    () {
+      test('Routes test', () async {
+        final routes = await provider.getRoutes();
+        expect(routes, isNotNull);
+        expect(routes, isA<List>());
+        routes.forEach((route) {
+          expect(route, isNotNull);
+          expect(route, isA<ShuttleRoute>());
+        });
+      });
+
+      test('Stops test', () async {
+        final stops = await provider.getStops();
+        expect(stops, isNotNull);
+        expect(stops, isA<List>());
+        stops.forEach((stop) {
+          expect(stop, isNotNull);
+          expect(stop, isA<ShuttleStop>());
+        });
+      });
+
+      test('Updates test', () async {
+        final updates = await provider.getUpdates();
+        expect(updates, isNotNull);
+        expect(updates, isA<List>());
+        updates.forEach((update) {
+          expect(update, isNotNull);
+          expect(update, isA<ShuttleUpdate>());
+        });
+      });
+
+      test('Connection test', () async {
+        // Dummy call to check the connection
+        provider.getRoutes();
+        final connected = provider.getIsConnected;
+        expect(connected, true);
+      });
+    },
+  );
+}

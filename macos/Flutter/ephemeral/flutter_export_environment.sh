eba11040e52506dc5134ab1df202efa03b44059a
Samuel Obe <samuel.obe@outlook.com>
2021-04-17 12:32:39 -0400

    Added current coverage report

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/samobe/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/samobe/Dev/CrossPlatform/flutter_shuttletracker"
+export "FLUTTER_APPLICATION_PATH=/Users/samobe/Dev/CrossPlatform/Flutter_ShuttleTracker"
+export "FLUTTER_TARGET=/Users/samobe/Dev/CrossPlatform/Flutter_ShuttleTracker/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "EXCLUDED_ARCHS=arm64"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/samobe/Dev/CrossPlatform/Flutter_ShuttleTracker/.dart_tool/package_config.json"
